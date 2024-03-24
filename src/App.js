a1157904ca1edc13764aab0535ba3370aa415e81
Oğuzhan Garip <oguzhan.garip21@gmail.com>
2024-03-24 18:50:48 +0300

    useRef oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
 
 function App() {
+  const [sayac, sayacGuncelle] = useState(0)
+
+  const puan = useRef(0)
+
+  function puanGuncelle() {
+    puan.current += 10
+  }
+
   return (
-    <div >
-      App
+    <div>
+      Puan: {puan.current}
+
+      <div>
+        {sayac}
+        <button onClick={ ()=>{ sayacGuncelle(eskiDeger=>eskiDeger+5) } }>Sayaç Arttır</button>
+      </div>
+      <div>
+        <button onClick={puanGuncelle}>Ref Değeri Güncelle</button>
+      </div>
     </div>
   );
 }
